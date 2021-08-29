fb33b849038b42a055a6ca75efe2f1555d220bc8
kalille Overton <>
2021-08-29 10:26:00 -0400

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,22 +9,33 @@ end
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 def monopoly_with_second_tier
- base_hash[:pieces]=4
- binding.pry
+  base = base_hash
+ base[:railroads][:pieces]=4
+ base[:railroads][:rent_in_dollars]={}
+ base[:railroads][:names]={}
+ base
 end
-
+  # When you start writing the implementation for this method, copy the Hash
+  # you built in the previous method. Run the tests. They will fail, but
+  # they'll guide you in how to modify what you just did to have more
+  # complexity.
 def monopoly_with_third_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+  second_tier = monopoly_with_second_tier
+  second_tier[:railroads][:rent_in_dollars]={:one_piece_owned=>25,:two_pieces_owned=>50,:three_pieces_owned=>100,:four_pieces_owned=>200}
+
+
+second_tier
 
 end
-
-def monopoly_with_fourth_tier
-  # When you start writing the implementation for this method, copy the Hash
+ # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+def monopoly_with_fourth_tier
+ 
+fourth_tier = monopoly_with_third_tier
+fourth_tier[:railroads][:names][:reading_railroad]={}
 
+fourth_tier
+binding.pry
 end
