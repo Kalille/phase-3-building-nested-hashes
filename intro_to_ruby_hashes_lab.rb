8df334e37a52ad0d07e51e0689e011de860335b0
kalille Overton <>
2021-08-29 10:47:45 -0400

    completed assignment

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,7 @@ end
 def monopoly_with_third_tier
   second_tier = monopoly_with_second_tier
   second_tier[:railroads][:rent_in_dollars]={:one_piece_owned=>25,:two_pieces_owned=>50,:three_pieces_owned=>100,:four_pieces_owned=>200}
-  second_tier[:railroads][:names]={:reading_railroad={}}
+  second_tier[:railroads][:names]={:reading_railroad=>{}, :pennsylvania_railroad=>{} ,:b_and_o_railroad=>{} , :shortline_railroad=>{}}
 
 second_tier
 
@@ -34,8 +34,10 @@ end
 def monopoly_with_fourth_tier
  
 fourth_tier = monopoly_with_third_tier
-fourth_tier[:railroads][:names][:reading_railroad]={}
+fourth_tier[:railroads][:names][:reading_railroad]={:mortgage_value=> 100}
+fourth_tier[:railroads][:names][:pennsylvania_railroad]={:mortgage_value=> 200}
+fourth_tier[:railroads][:names][:b_and_o_railroad]={:mortgage_value=> 400}
+fourth_tier[:railroads][:names][:shortline_railroad]={:mortgage_value=> 800}
 
 fourth_tier
-binding.pry
 end
